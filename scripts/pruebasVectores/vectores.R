f73c38b99112fcc107f9bbb971b2a5b091043528
marcelocussetti <mbussetti@4ns.com.ar>
2020-10-14 10:57:51 -0300

    vectores en R

---
+++ scripts/pruebasVectores/vectores.R
@@ -0,0 +1,38 @@
+#Tipos de datos
+##Vectores
+
+###formas de definirlos:
+
+vec1 = c(1,2,3,4,5,6,7)
+vec1
+
+#para armar un vecor a mano... introduciendo los valores por consola
+#tambien se usa para incorporar datos desde una url o archivos locales
+vec2 = scan("D:\\estadísticaDescriptiva\\r-basic\\scripts\\pruebasVectores\\miDataSet.txt")
+vec2
+
+#modificar un vector preexistente
+vec3 = fix(vec1)
+vec3
+
+#generar un vector en base a una repetición
+vec4 = rep("koko",9)
+vec4
+
+#para saber que tipo de datos contiene un vector se usa:
+class(vec1)
+
+
+##ejercicio 1: repite el año de tu nacimiento 10 veces
+anio10 = rep(1975, 10)
+anio10
+
+##ejercicio 2: Crea un vector con lo valores 16 0 1 20 1 7 88 5 1 9 llámalo vec 
+##y modifica la cuarta entrada con la función fix()
+
+vec = scan("D:\\estadísticaDescriptiva\\r-basic\\scripts\\pruebasVectores\\miDataSet.txt")
+vec
+
+vecMod = fix(vec)
+vecMod
+
